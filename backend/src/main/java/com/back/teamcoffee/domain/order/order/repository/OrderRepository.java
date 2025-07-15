f23fb7f9591e85b1f63397a1a044361d51d3cdf7
CYW815 <cho0815y@gmail.com>
2025-07-15 11:07:31 +0900

    Swagger 추가

--- backend/src/main/java/com/back/teamcoffee/domain/order/order/repository/OrderRepository.java
+++ backend/src/main/java/com/back/teamcoffee/domain/order/order/repository/OrderRepository.java
@@ -1,7 +1,7 @@
-package com.back.teamcoffee.domain.order.order.repository;
-
-import com.back.teamcoffee.domain.order.order.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
+//package com.back.teamcoffee.domain.order.order.repository;
+//
+//import com.back.teamcoffee.domain.order.order.entity.Order;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface OrderRepository extends JpaRepository<Order, Long> {
+//}
