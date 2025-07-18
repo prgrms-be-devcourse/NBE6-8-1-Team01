cf760d530b09dccbeff68fbafdbdf0becc21edf4
Jeongmin Lim <simount3@naver.com>
2025-07-18 11:24:47 +0900

    ㅕuser-order email 연결

--- backend/src/main/java/com/back/teamcoffee/domain/order/order/entity/Order.java
+++ backend/src/main/java/com/back/teamcoffee/domain/order/order/entity/Order.java
@@ -1,6 +1,7 @@
 package com.back.teamcoffee.domain.order.order.entity;
 
 import com.back.teamcoffee.domain.order.orderItem.entity.OrderItem;
+import com.back.teamcoffee.domain.user.entity.User;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Builder;
@@ -58,6 +59,11 @@ public class Order {
 
   @Column(length = 50)
   private String email;
+
+  // 🔽 연관관계 추가: ManyToOne → User.email 기준
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "email", referencedColumnName = "email", insertable = false, updatable = false)
+  private User user;
 
   @OneToMany(mappedBy = "order", fetch = LAZY, cascade = {PERSIST, REMOVE}, orphanRemoval = true)
   @JsonManagedReference
