211510d43e05825b2843ab8daf04308640890306
Jeongmin Lim <simount3@naver.com>
2025-07-18 10:13:59 +0900

    dto 어노테이션 추가 (#61)
    * 위시리스트  dto validation
    
    * 위시리스트 dto 어노테이션

--- backend/src/main/java/com/back/teamcoffee/domain/wishlist/dto/WishListUpdateDto.java
+++ backend/src/main/java/com/back/teamcoffee/domain/wishlist/dto/WishListUpdateDto.java
@@ -1,5 +1,8 @@
 package com.back.teamcoffee.domain.wishlist.dto;
 
+import jakarta.validation.constraints.Min;
+
 public record WishListUpdateDto(
+    @Min(value = 1, message = "수량은 1 이상이어야 합니다.")
     int quantity
 ) {}
